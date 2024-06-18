748d3137808e1716f3820da70f92341cd15608d7
Júlia Lima <juliafclima@hotmail.com>
2024-06-18 14:47:56 -0300

    Estrutura da Home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,23 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import { AppProps } from "next/app";
+import { globalStyles } from "../styles/global";
+
+import logoImg from "../assets/logo.svg";
+import { Container, Header } from "../styles/pages/app";
+
+import Image from "next/future/image";
+
+globalStyles();
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  );
 }
 
-export default MyApp;
+export default App;
